1a00a99ee1def7fd38ca9e4e83b588172074833c
0xdbe <23268141+0xdbe@users.noreply.github.com>
2021-09-14 08:30:09 +0200

    update random provider

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azuread = {
       source = "hashicorp/azuread"
-      version = "=1.0.0"
+      version = ">=1.0.0"
     }
     random = {
       source = "hashicorp/random"
-      version = "~>3.0.0"
+      version = ">=3.1.0"
     }
   }
   required_version = ">= 0.13"
